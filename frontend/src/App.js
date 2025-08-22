0515dc67c372a47bf9709338eea134d11d45e3ef
jangilipavani <23215A6703@bvrit.ac.in>
2025-08-22 14:08:33 +0530

    Add React frontend application with initial setup

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1>Hotel Management System</h1>
+      <p>Welcome to our hotel management system!</p>
+    </div>
+  );
+}
+
+export default App;
